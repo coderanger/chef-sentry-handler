e86ab3da9ab3cd683bd724ff66a0168f2febe539
Noah Kantrowitz <noah@coderanger.net>
2014-07-28 18:58:39 -0700

    Bump version for real release.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Report failed runs to Sentry"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
+version           "1.0.0"
 
 depends "chef_handler"
