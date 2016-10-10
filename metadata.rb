8e1a15094bbda7969b4fb5429fbceaad9e1296c8
John Bellone <jbellone@bloomberg.net>
2016-10-10 17:06:10 -0400

    Remove poise-derived from the run-list.

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description 'Report failed runs to Sentry.'
 version '2.0.0'
 
 depends 'chef_handler', '~> 2.0'
-depends 'poise-derived', '~> 1.0'
 
 chef_version '>= 12.1'
 issues_url 'https://github.com/coderanger/chef-sentry-handler/issues'
